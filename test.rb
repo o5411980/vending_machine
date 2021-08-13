e23a8782d8243845bb5a19cb627dbbcd4485fa92
yoichi ogo <o541.1980@gmail.com>
2021-08-13 21:11:42 +0900

    8/13日分 step3(修正),4,5

--- test.rb
+++ test.rb
@@ -48,11 +48,11 @@ class VendingMachine
     5.times {@drinks.push(Drink.cola)}
   end
 
-  def stocks
+  def stocks #vm.stocks　在庫(インスタンス自体)表示
     @drinks
   end
 
-  def stock_info #vm.stock_info
+  def stock_info #vm.stock_info　在庫リスト表示
     cola_count = 0
     redbull_count = 0
     water_count = 0
@@ -68,7 +68,7 @@ class VendingMachine
     @drinks.push(drink)
   end
 
-  def sales_count
+  def sales_count #vm.sales_count で売上額表示
     @sale_amount
   end
 
@@ -80,7 +80,7 @@ class VendingMachine
 #    end
 #  end
 
-  def which_can_buy
+  def which_can_buy #vm.which_can_buyで購入可能リスト表示
     if (@slot_money >= Drink.cola.price) && (@stock_list[:cola] >= 1)
       puts "#{Drink.cola.name}の価格#{Drink.cola.price}円、投入金額#{@slot_money}円、在庫#{@stock_list[:cola]}、購入できます。"
     end
@@ -92,7 +92,7 @@ class VendingMachine
     end
   end
 
-  def purchase_cola
+  def purchase_cola #vm.purchase_colaでcola購入
     if (@slot_money >= Drink.cola.price) && (@stock_list[:cola] >= 1)
       puts "投入金額#{@slot_money}円、在庫#{@stock_list[:cola]}、1本購入しました。"
       @sale_amount += Drink.cola.price
@@ -141,14 +141,14 @@ class VendingMachine
   end
 
   # 投入金額の総計を取得できる。
-  def current_slot_money
+  def current_slot_money #vm.current_slot_moneyで入金額表示
     # 自動販売機に入っているお金を表示する
     @slot_money
   end
 
   # 10円玉、50円玉、100円玉、500円玉、1000円札を１つずつ投入できる。
   # 投入は複数回できる。
-  def slot_money(money)
+  def slot_money(money) #vm.slot_money(100)で入金
     # 想定外のもの（１円玉や５円玉。千円札以外のお札、そもそもお金じゃないもの（数字以外のもの）など）
     # が投入された場合は、投入金額に加算せず、それをそのまま釣り銭としてユーザに出力する。
     return false unless MONEY.include?(money)
@@ -157,7 +157,7 @@ class VendingMachine
   end
 
   # 払い戻し操作を行うと、投入金額の総計を釣り銭として出力する。
-  def return_money
+  def return_money # vm.return_moneyで返金
     # 返すお金の金額を表示する
     puts "#{@slot_money}円のお返しです。"
     # 自動販売機に入っているお金を0円に戻す
@@ -176,7 +176,7 @@ class FiveTimesStore < VendingMachine
 end
 
 class PurchaseAndReturn < VendingMachine
-  def purchase_cola_and_return
+  def purchase_cola_and_return #vm=PurchaseAndReturn.newして、vm.purchase_cola_and_return
     purchase_cola
     return_money
   end
