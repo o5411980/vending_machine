92bcc8cf860684d79fa25c35d15acba0fb47224a
yoichi ogo <o541.1980@gmail.com>
2021-10-01 23:29:04 +0900

    usersにadminカラム追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "documents#index"
+  root to: "documents#new"
   resources :documents
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
