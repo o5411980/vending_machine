4a4489f4579c535d502593de728fdd5f9ad3990c
yoichi ogo <o541.1980@gmail.com>
2021-10-03 19:53:07 +0900

    category, control_number, authorizeカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_03_065413) do
+ActiveRecord::Schema.define(version: 2021_10_03_104927) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,9 @@ ActiveRecord::Schema.define(version: 2021_10_03_065413) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "filepath"
+    t.integer "category"
+    t.integer "control_number"
+    t.boolean "authorize"
   end
 
   create_table "users", force: :cascade do |t|
