2d918c82dd81716dfb9ed157f31ed82e8f1e1179
yoichi ogo <o541.1980@gmail.com>
2021-10-03 15:59:25 +0900

    documentsテーブルにfilepathカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_01_135351) do
+ActiveRecord::Schema.define(version: 2021_10_03_065413) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2021_10_01_135351) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "filepath"
   end
 
   create_table "users", force: :cascade do |t|
