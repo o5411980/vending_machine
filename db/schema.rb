ab494c7907eb160f406da8fd8969295fa2f4cf91
yoichi ogo <o541.1980@gmail.com>
2021-10-05 17:20:45 +0900

    scaffoldでprojectのCRUDを実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_05_075756) do
+ActiveRecord::Schema.define(version: 2021_10_05_081346) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,14 @@ ActiveRecord::Schema.define(version: 2021_10_05_075756) do
     t.string "description"
     t.integer "status"
     t.integer "location"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "projects", force: :cascade do |t|
+    t.string "name"
+    t.integer "status"
+    t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
