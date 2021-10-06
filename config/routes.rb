b5d211c8498405e97aa013c4e3d7a017557f3673
yoichi ogo <o541.1980@gmail.com>
2021-10-06 17:04:59 +0900

    Userモデルにcontrollerとview追加してCRUD機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :products
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
+  resources :users, only: [:show]
   root to: "documents#new"
   resources :documents
   if Rails.env.development?
