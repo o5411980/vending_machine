a50bd10bc76d4ecfa868f91bf5903d6b9088bef8
yoichi ogo <o541.1980@gmail.com>
2021-10-09 14:48:24 +0900

    users#updateのルーティングと処理を記述。モーダルからadmin_message保存まで実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,16 @@ class UsersController < ApplicationController
     @user = current_user
 #    @user = User.find(params[:id])
   end
+
+  def update
+  #Ajacsで管理者メッセージ更新。カラム追加したら comment → admin_messageに変更
+  @users = User.all
+  @user = current_user
+  @user.update(admin_message: homes_params)
+  end
+
+  private
+  def homes_params
+    params.require(:user).permit(:admin_message)
+  end
 end
