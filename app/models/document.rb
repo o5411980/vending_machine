b5bce16b453e2a8096d60a1cfcf663c24376715e
yoichi ogo <o541.1980@gmail.com>
2021-10-12 11:25:17 +0900

    Documentにenumを設定

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,4 +1,7 @@
 class Document < ApplicationRecord
   mount_uploader :filepath, DocumentUploader
   belongs_to :product
+
+  enum category: {'その他文書': 0, 'ビジネスプラン': 1}
+  enum authorize: {'未承認': false, '承認済': true}
 end
