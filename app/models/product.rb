683c7315c0ec8fd40caf30b684e5a9dc74e8e104
yoichi ogo <o541.1980@gmail.com>
2021-10-12 20:17:55 +0900

    Productにenum設定

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,7 @@
 class Product < ApplicationRecord
   has_many :documents, dependent: :destroy
+
+  enum category: {'マウス': 1, 'キーボード': 2, 'バッテリー': 3}
+  enum status: {'開発中': 0, '製造中': 1, '製造中止': 2}
+  enum location: {'未定': 0, '東京': 1, '深圳': 2, 'バンコク': 2, 'デュッセルドルフ': 3, 'ダラス': 4}
 end
